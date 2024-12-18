5d5b1cb93fde39538bb07db948833886b4e764f2
Henry <henrysavit@gmail.com>
2024-12-18 11:39:58 -0500

    upgrade eslint to v9

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import eslint from "@eslint/js"
+import tseslint from "@typescript-eslint/eslint-plugin"
+import tsparser from "@typescript-eslint/parser"
+import reactPlugin from "eslint-plugin-react"
+
+export default [
+	eslint.configs.recommended,
+	{
+		files: ["**/*.{ts,tsx}"],
+		ignores: ["dist/**/*", "node_modules/**/*"],
+		languageOptions: {
+			parser: tsparser,
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "module",
+				ecmaFeatures: {
+					jsx: true
+				}
+			}
+		},
+		plugins: {
+			"@typescript-eslint": tseslint,
+			react: reactPlugin
+		},
+		rules: {
+			"react/prop-types": "off",
+			"react/react-in-jsx-scope": "off"
+		},
+		settings: {
+			react: {
+				version: "detect"
+			}
+		}
+	}
+]
